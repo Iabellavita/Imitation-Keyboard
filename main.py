c29bae3ab785dcb375a0be33912b11069a78a1d0
labellavita <92790526+Iabellavita@users.noreply.github.com>
2022-04-11 19:04:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pyfiglet import Figlet
 import keyboard
 import random
+import time
 
 TIME_SLEEP = ['0.04', '0.05', '0.06', '0.07', '0.08', '0.09', '0.1', '0.2', '0.3']
 
@@ -24,16 +25,38 @@ if __name__ == "__main__":
     text = preview_text.renderText('KEYBOARD  IMITATION')
     print(f'\033[35m\033[1m{text}\033[0m')
     print(
-        "\033[33m\033[1m\033[4m#1\033[0m---\033[32m\033[1mINPUT YOUR TEXT TO \033[31m\033[4mtext.txt\033[0m \033[32m\033[1mFILE\033[0m\n"
-        "\033[33m\033[1m\033[4m#2\033[0m---\033[32m\033[1mDEFINE MOUSE CURSOR ON AREA AND CLICK LEFT BUTTON\033[0m\n"
-        "\033[33m\033[1m\033[4m#3\033[0m---\033[32m\033[1mPRESS \033[31m\033[4mF1\033[0m \033[32m\033[1mFOR START IMITATION\033[0m\n"
-        "\033[33m\033[1m\033[4m#4\033[0m---\033[32m\033[1mPRESS \033[31m\033[4mF2\033[0m \033[32m\033[1mFOR STOP IMITATION\033[0m\n")
-
+        "\033[33m\033[1m\033[4m[WARNING]\033[0m---\033[32m\033[1mINPUT YOUR TEXT TO \033[31m\033[4mtext.txt\033[0m \033[32m\033[1mFILE\033[0m\n"
+        "[\033[33m\033[1m\033[4m1\033[0m]---\033[32m\033[1mSTART SCRIPT AFTER \033[31m\033[4m4 SECONDS\033[0m\n"
+        "[\033[33m\033[1m\033[4m2\033[0m]---\033[32m\033[1mSTART SCRIPT AFTER PRESS \033[31m\033[4mHOT KEY\033[0m\n")
     try:
-        while True:
-            if keyboard.is_pressed('f1'):
-                insert_imitation_text()
-            if keyboard.is_pressed('f2'):
-                exit()
-    except:
+        script = int(input(
+            '\033[33m\033[1m\033[4m[ACTION]\033[0m \033[32m\033[1mPlease, choose options\033[0m\033[31m\033[1m [ 1 or 2 ]\033[0m: '))
+
+        if script == 2:
+            print(
+                "\033[33m\033[1m\033[4m[INFO]\033[0m---\033[32m\033[1mPRESS \033[31m\033[4mF1\033[0m \033[32m\033[1mFOR START IMITATION\033[0m\n"
+                "\033[33m\033[1m\033[4m[INFO]\033[0m---\033[32m\033[1mPRESS \033[31m\033[4mF2\033[0m \033[32m\033[1mFOR STOP IMITATION\033[0m\n")
+            try:
+                while True:
+                    if keyboard.is_pressed('f1'):
+                        insert_imitation_text()
+                    if keyboard.is_pressed('f2'):
+                        exit()
+            except:
+                print("\n\033[31m\033[1m[ERROR]\033[0m PROGRAM STOPPED BY USER\n")
+
+        if script == 1:
+            print(
+                "\033[33m\033[1m\033[4m[INFO]\033[0m---\033[32m\033[1mPROGRAM START IMITATION AFTER \033[31m\033[4m4 SECONDS\033[0m\n"
+                "\033[33m\033[1m\033[4m[INFO]\033[0m---\033[32m\033[1mPRESS \033[31m\033[4mF2\033[0m \033[32m\033[1mFOR STOP IMITATION\033[0m")
+            try:
+                while True:
+                    time.sleep(4)
+                    insert_imitation_text()
+                    if keyboard.is_pressed('f2'):
+                        exit()
+            except:
+                print("\n\033[31m\033[1m[ERROR]\033[0m PROGRAM STOPPED BY USER\n")
+
+    except KeyboardInterrupt:
         print("\n\033[31m\033[1m[ERROR]\033[0m PROGRAM STOPPED BY USER\n")
