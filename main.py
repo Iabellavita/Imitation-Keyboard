f059b14b0a5fc3f455a5bd1faa0e1d16e482f241
labellavita <92790526+Iabellavita@users.noreply.github.com>
2022-04-12 12:31:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from colorama import init
 from pyfiglet import Figlet
 import keyboard
 import random
 import string
 import time
 
+init()
 TIME_SLEEP = ['0.04', '0.05', '0.06', '0.07', '0.08', '0.09', '0.1', '0.2', '0.3']
 
 
